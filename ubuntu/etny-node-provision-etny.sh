9cad86085564329aa18e1af05244e5cd137eadaf
root <root@ns3205815.ip-141-94-195.eu>
2022-05-31 09:25:59 +0000

    asd

--- ubuntu/etny-node-provision-etny.sh
+++ ubuntu/etny-node-provision-etny.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/vagrant/etny/node/
 #git clone https://github.com/ethernity-cloud/mvp-pox-node etny-repo
-git clone https://github.com/nutriazx/testing-node.git
+git clone https://github.com/nutriazx/testing-node.git etny-repo
